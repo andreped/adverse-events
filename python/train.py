9db668f65a76c8ee19813b2ab519c17da5e41011
andreped <andrped94@gmail.com>
2021-05-10 14:44:57 +0000

    Added requirements file

---
+++ python/train.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+negative_data_path = "../data/AE_data/EQS_files/"
+positive_data_path = "../data/AE_data/2020_03_04_Uttrekk_kateter_fra_2015_uten_id.csv"
+save_model_path = "../output/models/"
+history_path = "../output/history/"
+datasets_path = "../output/datasets/"
+
+
+
+
+
